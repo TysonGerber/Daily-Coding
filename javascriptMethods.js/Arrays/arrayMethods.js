c7abd853dd3d6c2ba097863bc361dad96280d3fd
TysonGerber <tysonrgerber@gmail.com>
2017-03-02 11:19:56 -0700

    saving my notes on closures

--- javascriptMethods.js/Arrays/arrayMethods.js
+++ javascriptMethods.js/Arrays/arrayMethods.js
@@ -1,71 +1,62 @@
 //
-<=== ARRAY METHODS ===>
-arr.toString()
-//changes the elements in the array to a string (leaves the commas)
-
-function Car(brand, name, color, fuel, speed, year ) {
-  this.name = name;
-  this.brand = brand;
-  this.color = color;
-  this.fuel = fuel;
-  this.speed = speed;
-  this.year = year;
-}
-
-fordFocus = new Car("Ford", "Focus", "Blue", "Regular Unleaded", "80 mph", "2002");
-
-Car.prototype.toString = function carToString(){
-  return " This is a " + this.color + " " + this.brand + " " + this.name + ". " + " It's reveal date was " + this.year + ", " + " it's max speed is " + this.speed + " and takes " + this.fuel + " fuel."
-}
-console.log(fordFocus.toString()) //This is a Blue Ford Focus.  It's reveal date was 2002,  it's max speed is 80 mph and takes Regular Unleaded fuel.
-console.log(fordFocus+" ") // same
+                             <=== ARRAY METHODS ===>
 
 
-arr.join()
-//easily converts an array to a clean string
-    var combine = ["H", "e", "l", "l", "o", " ", "A", "l", "e", "x", " ", "a", "n", "d", " ", "C", "h", "a", "r", "l", "i", "e"]
-        console.log(combine.join(""))
+// ***************************************************************************************
+// 								TYSON GERBER
+// ***************************************************************************************
 
-    var numbers = [1,2,3,4,5]
-    console.log(numbers.join(" + "));
+       1. arr.toString()
+ 
 
-arr.pop()
-//removes from the end of the array
-    var names = ["Alex", "Charlie", "Tyson", "Richard"]
-    console.log(names.pop());
+            fordFocus = new Car("Ford", "Focus", "Blue", "Regular Unleaded", "80 mph", "2002");
 
-arr.push()
-//adds to the end of the array
-    var names = ["Alex", "Charlie", "Tyson", "Richard"]
-   console.log(names.push("Rob"))
-   console.log(names)
+            Car.prototype.toString = function carToString(){
+              return " This is a " + this.color + " " + this.brand + " " + this.name + ". " + " It's reveal date was " + this.year + ", " + " it's max speed is " + this.speed + " and takes " + this.fuel + " fuel."
+            }
+            console.log(fordFocus.toString()) //This is a Blue Ford Focus.  It's reveal date was 2002,  it's max speed is 80 mph and takes Regular Unleaded fuel.
+            console.log(fordFocus+" ") // same
 
-arr.shift()
-//removes form the beggining of the array
-    	var dev = ["I", "am", "a", "FullStack", "Developer"]
-   		   console.log(dev.shift()) // I
-           console.log(dev) // [ 'am', 'a', 'FullStack', 'Developer' ]
+            // fordFocus is an object and you want to use it as an object, but if you want to print out the STRING (toString() output) rather than the OBJECT ({name:…, brand:…}) you need to use it as a string.  By adding a string to the object, it implicitly converts the object into the string using toString().
+            // console.log(fordFocus) outputs the object.
+            // console.log(fordFocus + “ “) outputs the string (and calls toString() to do that)
+            
+       2. arr.join() //The join() method joins all elements of an array (or an array-like object) into a string.
+        	var combine = ["H", "e", "l", "l", "o", " ", "A", "l", "e", "x", " ", "a", "n", "d", " ", "C", "h", "a", "r", "l", "i", "e"]
+           console.log(combine.join("")); //Hello Alex and Charlie
+            
+          var numbers = [1,2,3,4,5]
+           console.log(numbers.join(" + ")); //1 + 2 + 3 + 4 + 5
+        
+       3. arr.pop() //The pop() method removes the last element of an array, and returns that element.
+        	var names = ["Alex", "Charlie", "Tyson", "Richard"]
+           console.log(names.pop()); //Richard
+        
+       4. arr.push() //The push() method adds one or more elements to the end of an array and returns the new length of the array.
+          var names = ["Alex", "Charlie", "Tyson", "Richard"]
+           console.log(names.push("Rob")) // 5
+           console.log(names) //   ["Alex", "Charlie", "Tyson", "Richard", "Rob"]
+               
+               
+       5. arr.shift()  //The shift() method removes the first element from an array and returns that element. This method changes the length of the array.
+          var dev = ["I", "am", "a", "FullStack", "Developer"]
+   				 console.log(dev.shift()) // I
+           console.log(dev) //[ 'am', 'a', 'FullStack', 'Developer' ]
            console.log(dev.shift()) // am
-           console.log(dev) // [ 'a', 'FullStack', 'Developer' ]
+           console.log(dev) //[ 'a', 'FullStack', 'Developer' ]
            console.log(dev.shift()) // a
            console.log(dev) // [ 'FullStack', 'Developer' ]
-         
-           
-arr.unshift()
-//adds to the beggining of the array
-
-  console.log(dev.unshift("are")) // 3
+        
+       6. arr.unshift() //The unshift() method adds one or more elements to the beginning of an array and returns the new length of the new array.
+        	(continuing of the aobve shift example)
+           console.log(dev.unshift("are")) // 3
            console.log(dev) // [ 'are', 'FullStack', 'Developer' ]
            console.log(dev.unshift("We")) // 4
            console.log(dev) // [ 'We', 'are', 'FullStack', 'Developer' ]
-           
-
-arr.length
-//returns the number of items in the array
-
-var combine = ["H", "e", "l", "l", "o", " ", "A", "l", "e", "x", " ", "a", "n", "d", " ", "C", "h", "a", "r", "l", "i", "e"]
-console.log(combine.length)           
-
+        
+       7. arr.length //not a function()
+       		 var combine = ["H", "e", "l", "l", "o", " ", "A", "l", "e", "x", " ", "a", "n", "d", " ", "C", "h", "a", "r", "l", "i", "e"]
+         	 	console.log(combine.length) // (0-22 includes spaces as an index.) 22     
 
 delete arr[0]
 //will delete the item at the specified index of the array
@@ -86,3 +77,59 @@ map()
 
 forEach()
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+// =======================================================================================
+// 			          CHARLIE BECK
+// =======================================================================================
+//Closure Explained:
+
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Closures
+//https://www.youtube.com/watch?v=yiEeiMN2Khs
+
+
+//3. Work on these 7 Array Methods
+  // ==================== arr.toString() =======================
+  // Convert the array to a string.
+  arr = [45, "minutes", "to", "Boulder"];
+
+  console.log(arr.toString(arr)); // arr = "45, "minutes", "to", "Boulder"
+
+  // ==================== arr.join() ===========================
+
+  console.log(arr.join(" ")); // arr = "45 minutes to Boulder"
+
+  // ==================== arr.pop() ===========================
+
+  console.log(arr.pop()); //returns the last string in an array and deletes it. logs Boulder, returns arr = [45, "minutes", "to"]
+
+  // ==================== arr.push() ==========================
+
+  console.log(arr.push("Pine")); //log returns length. .push method adds to the end of an array.
+
+  // ==================== arr.shift() =========================
+
+  console.log(arr.shift()); //log returns the 0 index, .shift method removes the 0 index from array.
+
+  // ==================== arr.unshift() ======================
+
+  console.log(arr.unshift("35")); //log returns the length, .unshift method adds to beggining of array. Differs from .push() in that it adds to beggining instead of end.
+
+  // ==================== arr.length() =======================
+
+  console.log(arr.length); //.length returns length of the array.
+
+  console.log(arr); //after all this. arr ends up as arr = ["35", "minutes", "to", "Pine"]
+
+// =======================================================================================
---
+++ javascriptMethods.js/closures.js
@@ -0,0 +1,114 @@
+// *******************************************************************************************
+//                                         CLOSURES:
+// *******************************************************************************************
+// Closures:  A closure is a function with its own private store of variables, that no other function knows about or can access.
+// You can create any number of closures. even with the same variable name contained within each one of them.That's because the variables are hidden from all other functions.
+
+
+// References:  https://teamtreehouse.com/library/the-problem-with-globals
+//              http://speakingjs.com/es5/ch16.html#inadvertently_sharing_environments
+
+
+//    This is why we have closures. Below is not a closure but shows the need of why we need them!
+
+var count = 0;
+
+function countBirds() {
+    count += 1;
+    return count + ' birds';
+}
+
+function countDogs() {
+    count += 1;
+    return count + ' dogs';
+}
+
+//   in console type 
+//   >countBirds()
+//   >countBirds()
+//   >countBirds()
+//   the count dogs
+//   >countDogs()
+//   >countDogs()
+//   >countDogs()
+//   >countDogs()
+
+
+// so we would have to create seperate variables for each for this to work properly
+var dogCount = 0;
+var birdCount = 0;
+
+
+
+//What if we had 1,000 different things we wanted to count? Can you imagine how long it would take to write out a variable for each of these? Closures are what solve these problems! 
+
+var birds = 3;
+
+function dogHouse() {
+    var dogs = 8;
+    console.log(birds); // 3
+    console.log(dogs); //8
+}
+
+console.log(birds); //3
+console.log(dogs); // undefined
+
+function outer() {
+    var countDogs = 0
+    function inner() {
+        count++;
+    }
+    return outer
+}
+
+
+
+// Starting of a closure example:
+
+
+function dogHouse() { //Outer function
+    var dogs = 8;
+    function showDogs() { //Inner function. All variables in the outer funcation are visible to to the inner function. Even thouhg they are hidden from the global scope. Just like the outer func has access to the global variables. The inner function has access to the outer functions scope.
+        console.log(dogs)
+        return showDogs; //f the inner function is returned 
+    }
+}
+
+var getDogs = dogHouse(); //and assigned to a varialbe
+getDogs(); // the private scope of the outer function lives on and remains in the programs memory. So this means we can call the retunred inner function and get access or even modify any private variables.
+
+
+
+
+
+function outerFunction() {
+    var someCount = 0;
+    function innerFunction() {
+        someCount++;
+        console.log("Called " + someCount + "times");
+    }
+    return innerFunction;
+}
+
+counter1 = outerFunction();
+counter2 = outerFunction();
+counter1(); // Called 1 times
+counter2(); // Called 1 times
+
+
+
+function usersLocation(ipAddressLocation) {
+    var user = 0;
+    function userCount() {
+        user++;
+        console.log("So far we have " + user + " from "  + ipAddressLocation + ".")
+    }
+    return userCount
+}
+
+sanFranciscoUsers = usersLocation("San Francisco")
+DenverUsers = usersLocation("Denver")
+
+sanFranciscoUsers();
+sanFranciscoUsers();
+DenverUsers();
