6f5aabc3a1737757a293c16e503beba1edf02129
TysonGerber <tysonrgerber@gmail.com>
2017-03-04 22:18:58 -0700

    my daily code for the day.

--- Scope/scopeAndHoisting.js
+++ Scope/scopeAndHoisting.js
@@ -65,8 +65,7 @@ obj.getColor()  // green
 
 
 
-
-
+// Asynchronysity and when what runs where.
     var myvar = "global";
     function f() {
        console.log('One',myvar); // (*)
@@ -81,6 +80,7 @@ obj.getColor()  // green
 
 
 
+//Simple example of scope
 
 var x = "global";
 function f() {
@@ -94,4 +94,201 @@ console.log(x);
 
 
 
+var fullname = 'John Doe'; // Global Scope
+ var getFullname = function () {
+        return this.fullname;
+    }
 
+var obj = { // Global Scope
+    fullname: 'Colin Ihrig',
+    getFullname: function () {
+        return this.fullname;
+    },
+   prop: { // object (obj) Scope
+        fullname: 'Aurelio De Rosa',
+        getFullname: function () {
+            return this.fullname;
+        }
+    } // ./prop
+}; // ./obj
+
+console.log(fullname);
+console.log(obj.fullname);
+console.log(obj.prop.fullname)
+console.log(obj.prop.getFullname());
+console.log(obj.getFullname());
+console.log(getFullname());
+// 'John Doe' // Nope. 
+// 'Aurelio De Rosa' // This is the correct answer. prop is an object with two properties, fullname and getFullname. getFullname is a method, or a property that is also a function.
+// When obj.prop.getFullname() is called, it is essentially saying prop -> call your method getFullname on YOURSELF. Thus, the above statement returns prop's fullName property
+var test = obj.prop.getFullname;
+console.log(test());
+
+
+
+
+function f(){
+    var y=1;
+    //need to return
+}
+
+console.log(y) // this will be undefined because it is not in the variable scope.
+console.log(f()); // function doesn't return anything.
+
+
+
+// DO NOT DO THIS IT WILL LEAK!!!  
+// if there is no other variable assigned to y outside of local scope then it will be created but go to global scope and leak
+
+
+function f(){   
+     y=1;
+}
+
+console.log(y)
+
+   var myvar = "global";
+    function f() {
+        print(myvar); // (*)
+        if (true) {
+            var myvar = "local"; // (**)
+        }
+        print(myvar);
+    }
+
+     f();
+
+
+
+
+
+    function objMaker(color) {
+        return {
+            getColor: function() {
+                return color;
+            },
+            setColor: function(c) {
+                color = c;
+            }
+        };
+    }
+// Interaction:
+
+      var c = objMaker("blue");
+     console.log(c.getColor());
+  
+     c.setColor("green");
+     c.getColor()        
+
+ 
+ //HOISTING EXAMPLE:
+
+
+// because this is not set to a variable it can access var x=1 at the bottom of the function.
+ 
+//  HOISTING EXAMPLE ONE 
+ function getX(){
+     return x;
+ }
+
+ var x = 1
+console.log(x)
+
+y=2
+
+
+ function getX(){
+    return y=1
+ }
+
+ var x = 1
+console.log(x)
+console.log(y)
+console.log(getX())
+
+
+
+
+//  HOISTING EXAMPLE TWO 
+console.log(getX()) //cannot access here.
+
+var getX = function(){
+    return x;
+}
+var x = 1 //This has to be above console.log or it doesn't work
+
+console.log(getX()) // because it has a variable assigned to it. We can now access this here.
+
+
+
+
+// HOISTING EXAMPLE THREE
+
+var getX = function(){
+    return x;
+}
+
+
+ console.log(getX()) //Becomes undefined because x is not defined before we call it.
+
+var x = 1  
+
+
+
+
+
+
+
+
+
+
+var foo = 1;
+
+function bar() {
+	if (!foo) {
+		var foo = 10;
+	}
+	console.log('foo', foo);
+}
+console.log('function', bar());
+
+ 
+foo:1
+function bar ===== foo:10
+                    console.log(foo)
+
+console.log
+
+
+var x = 1;
+console.log(x); 
+
+if (true) {
+	var x = 2;
+	console.log(x); 
+}
+console.log(x);
+
+
+
+
+
+
+
+
+
+
+
+  
+function test() {
+   console.log(a); // undefined because the variable has not been declaired yet.
+   console.log(foo()); //because the function is not assigned to a variable this one will run. if it was a variable this would also be undefined.
+   
+   var a = 1;
+   function foo() {
+      return 2;
+   }
+}
+
+test();// undefined 
+  		// 2
---
+++ jsMethods/Interview questions/interviewPractice.js
@@ -0,0 +1,84 @@
+
+//http://www.logicguns.com/q/what-is-a-potential-pitfall-with-using-typeof-bar-objectto-determine-if-baris-an-object-how-can-this-pitfall-be-avoided/5625c8222b78726726a8e10a/
+  
+function foo1()
+{
+  return {
+      bar: "hello"
+  };
+}
+
+function foo2()
+{
+  return
+  {
+      bar: "hello"
+  };
+
+}
+  
+console.log(foo1());
+  console.log(typeof bar == 'object') && console.log(bar != null);
+console.log(foo2());
+  console.log(typeof bar == 'object') && console.log(bar != null);
+
+
+
+
+var name = null;
+var name2 = undefined;
+
+console.log(typeof name == "object") // {bar: "hello"}
+console.log(typeof name2 == "object") // undefined
+
+
+
+
+// Scope and how it works.
+  
+var fullname = 'John Doe'; // Global Scope
+var obj = { // Global Scope
+   fullname: 'Colin Ihrig',
+   prop: { // object (obj) Scope
+      fullname: 'Aurelio De Rosa',
+      getFullname: function() {
+         return this.fullname;
+      }
+   } // ./prop
+}; // ./obj
+
+console.log(obj.prop.getFullname());
+// 'Aurelio De Rosa' // This is the correct answer. prop is an object with two properties, fullname and getFullname. getFullname is a method, or a property that is also a function.
+// When obj.prop.getFullname() is called, it is essentially saying prop -> call your method getFullname on YOURSELF. Thus, the above statement returns prop's fullName property
+var test = obj.prop.getFullname; // This one is tricky. First, this line is essentially aliasing prop's getFullname method to make use of it in the global space. When Test() is called in the global namespace, it will be called on the first global variable it can find with the name 'fullname'
+// In this case, that global variable is 'John Doe', which is the answer. If fullname were called anything else, say 'var pecker = ...', you would then see undefined like you suggested.
+
+
+console.log(test()); //testes one two Undefined is the write answer! :) 
+  //1st guess was this but I think it is wrong:'Undefined' 
+
+//No sir: The code prints Aurelio De Rosa and John Doe. The reason is that the context of a function, what is referred with the this keyword, in JavaScript depends on how a function is invoked, not how it’s defined.
+
+// In the first console.log() call, getFullname() is invoked as a function of the obj.prop object. So, the context refers to the latter and the function returns the fullname property of this object. On the contrary, 
+// when getFullname() is assigned to the test variable, the context refers to the global object (window). This happens because test is implicitly set as a property of the global object. For this reason, the function 
+// returns the value of a property called fullname of window, which in this case is the one the code set in the first line of the snippet.
+// Have you actually written this script and tried it?
+
+  
+// what is the result of the three console log statements
+
+// What I understand is that the child can see outside to the parent. However the parent cannot see into the child. Example. 
+// Well, let me see that example! :P 
+// Correct.
+
+ write me a JavaScript that takes the array var arr = [1,2,3,4,5,6,7,8,9,10] and returns a new array that’s a copy of arr, but reversed. Do it. And no shortcut methods! 😛
+
+var numbers = [1,2,3,4,5,6,7,8,9,10]
+
+
+var newArr = numbers.reverse().slice()
+console.log(newArr)
+
+
+var numbers = [1,2,3,4,5,6,7,8,9,10]
+console.log(numbers.reverse())
