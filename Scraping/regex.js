a4cfb65c8d7fbe4cfa3b784db69348b72e4e2abc
TysonGerber <tysonrgerber@gmail.com>
2017-03-24 23:04:30 -0600

    got name\, licenses, email etc to be able to be scraped

--- Scraping/regex.js
+++ Scraping/regex.js
@@ -1,4 +1,13 @@
 var name = "Tyson Gerber<>hello"
 
-var name2 = name.match(/[a-z ]/gi)
-console.log(name2)
+// var name2 = name.match(/[a-z ]/gi)
+// console.log(name2)
+
+var eliminate = name.match((/[^%]*/i)[0])
+console.log(eliminate)
+
+
+var stringVariable = 'America/Argentina/Buenos_Aires',
+    text = stringVariable.substring(0, stringVariable.indexOf('/'));
+
+    console.log(text)
--- Scraping/server.js
+++ Scraping/server.js
@@ -7,67 +7,88 @@ var nightmare = Nightmare({ show: true });
 nightmare
     .goto('https://store.aacc.net/ccn/ccn_disclaimer.php')
     //.type()
-        // .click('#main > form:nth-child(5) > input')
-         //.type('form[action*="ccn_search.php"] [value=I Agree]')
-        //.click('form[action*="ccn_search.php"] [value=I Agree]')
-        .click('#main [value="I Agree"]')
-        .wait('#search')
-        .wait(2000)
-        .click('#search > form [title="CheckAll"]')
-        .wait('#search')
-        .wait(2000)
-        .click('#search > form > table > tbody > tr:nth-child(4) > td:nth-child(2) > input[type="submit"]')
-          .wait('#search')
-          .wait(2000)
-        .click('#search > form > table > tbody > tr:nth-child(2) > td:nth-child(3) > select')
-       
-        //    for(var i = 0; i < .length-15; i++){
-        //       .click('#search > form > table > tbody > tr:nth-child(4) > td:nth-child(2) > input[type="submit"]')
-        //    }
-          
-        //    .check('input [type=checkbox][value="IBC-BCPCC"]')
-        //    .check('input [type=checkbox][value="IBC-BCCC"]')
-        //    .check('input [type=checkbox][value="IBC-BCPC"]')
-        //    .check('input [type=checkbox][value="IBC-BCBC"]')
-        //    .wait('#main')
+    // .click('#main > form:nth-child(5) > input')
+    //.type('form[action*="ccn_search.php"] [value=I Agree]')
+    //.click('form[action*="ccn_search.php"] [value=I Agree]')
+    .click('#main [value="I Agree"]')
+    .wait('#search')
+    .click('#search > form [title="CheckAll"]')
+    .wait('#search')
+    .click('#search > form > table > tbody > tr:nth-child(4) > td:nth-child(2) > input[type="submit"]')
+    .wait('#search')
+    .click('#search > form > table > tbody > tr:nth-child(2) > td:nth-child(3) > select')
+
+    //    for(var i = 0; i < .length-15; i++){
+    //       .click('#search > form > table > tbody > tr:nth-child(4) > td:nth-child(2) > input[type="submit"]')
+    //    }
+
+    //    .check('input [type=checkbox][value="IBC-BCPCC"]')
+    //    .check('input [type=checkbox][value="IBC-BCCC"]')
+    //    .check('input [type=checkbox][value="IBC-BCPC"]')
+    //    .check('input [type=checkbox][value="IBC-BCBC"]')
+    //    .wait('#main')
 
 
 
-        // .mouseup('.right')
-    
-        // .type('input[type=submit]')
-        // .evaluate(function() {
-        //   console.log(document.querySelector('input'));
-        // })
-        
-        //.type('input[value="I Agree"]')
-        // .mousedown('.right')
-        //.click('.right')
-    
-        
-        // .type('form[action*="ccn_search.php"] [value=Disagree]')
-        //   .click('form[action*="ccn_search.php"] [value=Disagree]')
-        //    .exists('form[action*="ccn_search.php"] [value=Disagree]')
-       // .type('input[value=I Agree]')
+    // .mouseup('.right')
+
+    // .type('input[type=submit]')
+    // .evaluate(function() {
+    //   console.log(document.querySelector('input'));
+    // })
+
+    //.type('input[value="I Agree"]')
+    // .mousedown('.right')
+    //.click('.right')
+
+
+    // .type('form[action*="ccn_search.php"] [value=Disagree]')
+    //   .click('form[action*="ccn_search.php"] [value=Disagree]')
+    //    .exists('form[action*="ccn_search.php"] [value=Disagree]')
+    // .type('input[value=I Agree]')
     //.click('submit')
 
-      
+
     .wait('#results')
     // evaluate code IN THE NIGHTMARE BROWSER
     .evaluate(function () {
-    //    var info =[]
-        // pass data from the nightmare browser back to this express app
-      //console.log(document.body.querySelectorAll("#results tr td")[0]).innerText
-      console.log(document.body.querySelectorAll("#results tr td")[0].innerHTML)
+        var info = []
 
-      var name = document.body.querySelectorAll("#results tr td")[0].innerHTML
-      console.log(name.match(/[a-z ]/gi))
-      //document.body.querySelectorAll("#results tr td")[0].innerHTML
-      return name
-       //return document.body.querySelectorAll("#results tr")[1].innerHTML
-      
-    
-        
+        // This gets the Name of the client and degrees
+        info.push((document.body.querySelectorAll("#results tr td")[0].innerHTML))
+
+        // Licenses
+        info.push((document.body.querySelectorAll("#results tr td")[1].innerHTML))
+
+        // Not sure what this is BCPPC
+        info.push((document.body.querySelectorAll("#results tr td")[2].innerHTML))
+
+        // Email
+        info.push((document.body.querySelectorAll("#results tr td")[5].innerHTML))
+
+        //State or Country
+        info.push((document.body.querySelectorAll("#results tr td")[3].innerHTML))
+
+       
+        // var name = (document.body.querySelectorAll("#results tr td")[0].innerHTML)
+        // // var letters = name.match(/[a-z ]/gi)
+
+        // //give me everything up until the first instance of a "<" sign.
+        // var clientName = name.substring(0, name.indexOf('<'));
+
+        // //push the name into array info
+        // info.push(clientName)
+
+        // //console.log clientName
+        // console.log(clientName)
+
+        // //document.body.querySelectorAll("#results tr td")[0].innerHTML
+        return info
+
+        //return document.body.querySelectorAll("#results tr")[1].innerHTML
+
+
+
     })
 
     // close the browser window
