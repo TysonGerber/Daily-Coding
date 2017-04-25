c95449401ad6a8664602d1c46592f117b5f8eb3c
TysonGerber <tysonrgerber@gmail.com>
2017-04-24 22:02:00 -0600

    saving 2nd daily challenge. Now I just need to add the return - and 0 if the result overflows and doesn't fit in a 32 bit signed

--- funCode/fun.js
+++ funCode/fun.js
@@ -1,7 +1,37 @@
 // SendGrid submit a writting sample. (with no description. So I sent this. :) 
 
-function findIntern(name){
+function findIntern(name) {
     return "Send Grid would enjoy working with the Developer Intern " + name + "!"
 }
 
 console.log(findIntern("Tyson Gerber"));
+
+
+
+
+// Example1:
+
+// x = 123,
+
+// return 321
+// Example2:
+
+// x = -123,
+
+// return -321
+
+// Return 0 if the result overflows and does not fit in a 32 bit signed integer.
+let x = 123
+
+function backwards(x) {
+    var rev = x.toString().split("")
+    var backwards = [];
+    console.log(rev)
+    for (var i = rev.length-1; i >= 0 ; i--) {
+     backwards.push(rev[i])
+     
+    }
+    return backwards
+}
+
+console.log(backwards(x))
